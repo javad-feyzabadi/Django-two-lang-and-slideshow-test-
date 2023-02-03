343861e1deb5cced283a71565a1af70e3c3c5291
javad <javad.feyzabadi@yahoo.com>
2023-02-03 22:10:18 +0330

    create slideshow page and two-lang

---
+++ slideshow/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render,redirect
+from django.utils.translation import activate
+from . models import SlideShow
+
+
+def home(request):
+    context = {
+        'slides':SlideShow.objects.filter(status = True,article__status = True)
+    }
+    return render(request,'slideshow/home.html',context)
+
+def change_lang(request):
+    activate(request.GET.get('lang'))
+    return redirect(request.GET.get('next'))
