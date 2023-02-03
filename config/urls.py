726fcb84cf75a63cf60f9107e2fea2e4ccc93776
javad <javad.feyzabadi@yahoo.com>
2023-02-04 02:22:46 +0330

    google social

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,13 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
-from slideshow.views import change_lang
+from slideshow.views import change_lang,logout_view
 
 urlpatterns = [
     path('change_lang/',change_lang,name='change_lang'),
+    path('logout/',logout_view,name='logout'),
+    path('', include('social_django.urls', namespace='social')),
+
     
 ]
 urlpatterns += i18n_patterns (
